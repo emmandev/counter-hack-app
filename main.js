23609f9324a8d8421da010007a85b84c9b769b12
Emmanuel Tierra <emman.tierra@gmail.com>
2018-10-03 14:24:38 +0800

    Add code for counter

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-/* write your cool javascript code here */
+(function () {
+    var counter = document.getElementById('counter');
+
+    document.querySelectorAll('input[type="button"]').forEach(function (el) {
+        el.addEventListener('click', function (e) {
+            if (value === 'Increment') {
+                counter.value = (+ counter.value) + 1;
+            }
+            else if (value == 'Decrement') {
+                counter.value = (+ counter.value) - 1;
+            }
+        });
+    });
+})();
